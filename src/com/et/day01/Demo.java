cdb6649831293a1bb62813c91946c77d644fa023
Demo <1470712511@qq.com>
2018-01-01 21:42:49 +0800

    Structs

---
+++ src/com/et/day01/Demo.java
@@ -0,0 +1,8 @@
+package com.et.day01;
+
+public class Demo {
+	public String execute(){
+		
+		return "ok";
+	}
+}
---
+++ src/com/et/day01/Hello.java
@@ -0,0 +1,8 @@
+package com.et.day01;
+
+
+public class Hello{
+	public String execute() {
+		return "bb";
+	}
+}
